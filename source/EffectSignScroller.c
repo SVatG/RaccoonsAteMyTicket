195ce8e9a08f66ade825c37b687311ae4a4a3bd9
PoroCYon <p@pcy.be>
2023-07-16 19:32:11 +0200

    bleh

--- source/EffectSignScroller.c
+++ source/EffectSignScroller.c
@@ -11,12 +11,12 @@
 
 #include "Tools.h"
 #include "GraphicsLibrary/MonoFont.h"
- 
+
 // Global data: Matrices
 static int uLocProjection;
-static int uLocModelview; 
+static int uLocModelview;
 static C3D_Mtx projection;
- 
+
 // Global data: Lighting
 static C3D_LightEnv lightEnv;
 static C3D_Light light;
@@ -99,11 +99,11 @@ void effectSignScrollerInit() {
     // Prep general info: Shader (precompiled in main for important ceremonial reasons)
     C3D_BindProgram(&shaderProgramBones);
 
-    // Prep general info: Uniform locs 
+    // Prep general info: Uniform locs
     uLocProjection = shaderInstanceGetUniformLocation(shaderProgramBones.vertexShader, "projection");
     uLocModelview = shaderInstanceGetUniformLocation(shaderProgramBones.vertexShader, "modelView");
 
-    // Prep general info: Bones 
+    // Prep general info: Bones
     char boneName[255];
     for(int i = 0; i < 21; i++) {
         sprintf(boneName, "bone%02d", i);
@@ -167,22 +167,22 @@ static void drawModel(fbxBasedObject* model, float row) {
     BufInfo_Add(bufInfo, (void*)model->vbo, sizeof(vertex_rigged), 5, 0x43210);
 
     // Bind texture
-    C3D_TexBind(0, model->tex);    
+    C3D_TexBind(0, model->tex);
     C3D_TexSetFilter(model->tex, GPU_LINEAR, GPU_LINEAR);
 
     // Set up lighting
     C3D_LightEnvInit(&lightEnv);
     C3D_LightEnvBind(&lightEnv);
-    
+
     LightLut_Phong(&lutPhong, 100.0);
     C3D_LightEnvLut(&lightEnv, GPU_LUT_D0, GPU_LUTINPUT_LN, false, &lutPhong);
-    
+
     // Add funny edge lighting that makes 3D pop
     float lightStrengthFresnel = 0.5;
     LightLut_FromFunc(&lutShittyFresnel, badFresnel, lightStrengthFresnel, false);
     C3D_LightEnvLut(&lightEnv, GPU_LUT_FR, GPU_LUTINPUT_NV, false, &lutShittyFresnel);
     C3D_LightEnvFresnel(&lightEnv, GPU_PRI_SEC_ALPHA_FRESNEL);
-    
+
     // Basic shading with diffuse + specular
     C3D_FVec lightVec = FVec4_New(0.0, 0.0, 0.0, 1.0);
     C3D_LightInit(&light, &lightEnv);
@@ -205,8 +205,8 @@ static void drawModel(fbxBasedObject* model, float row) {
     C3D_TexEnvSrc(env, C3D_RGB, GPU_TEXTURE0, GPU_FRAGMENT_PRIMARY_COLOR, 0);
     C3D_TexEnvOpRgb(env, 0, 0, 0);
     C3D_TexEnvFunc(env, C3D_RGB, GPU_MODULATE);
-    
-    env = C3D_GetTexEnv(1); 
+
+    env = C3D_GetTexEnv(1);
     C3D_TexEnvInit(env);
     C3D_TexEnvSrc(env, C3D_RGB, GPU_FRAGMENT_SECONDARY_COLOR, GPU_PREVIOUS, 0);
     C3D_TexEnvOpRgb(env, GPU_TEVOP_RGB_SRC_ALPHA , 0, 0);
@@ -215,13 +215,13 @@ static void drawModel(fbxBasedObject* model, float row) {
     env = C3D_GetTexEnv(2);
     C3D_TexEnvInit(env);
     C3D_TexEnvSrc(env, C3D_Alpha, GPU_CONSTANT, GPU_PREVIOUS, 0);
-    C3D_TexEnvFunc(env, C3D_Alpha, GPU_REPLACE);  
+    C3D_TexEnvFunc(env, C3D_Alpha, GPU_REPLACE);
 
-    // GPU state for normal drawing with transparency 
+    // GPU state for normal drawing with transparency
     C3D_DepthTest(true, GPU_GEQUAL, GPU_WRITE_ALL);
     C3D_CullFace(GPU_CULL_BACK_CCW);
     C3D_AlphaBlend(GPU_BLEND_ADD, GPU_BLEND_ADD, GPU_SRC_ALPHA, GPU_ONE_MINUS_SRC_ALPHA, GPU_SRC_ALPHA, GPU_ONE_MINUS_SRC_ALPHA);
- 
+
     // Actual drawcall
     C3D_DrawArrays(GPU_TRIANGLES, 0, model->vertCount);
 }
@@ -232,17 +232,17 @@ void effectSignScrollerRender(C3D_RenderTarget* targetLeft, C3D_RenderTarget* ta
     const char *compo = COMPOS[index];
     signSetStrings(compo, compo);
 
-    // Frame starts (TODO pull out?)   
+    // Frame starts (TODO pull out?)
     C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
- 
-    // Send modelview 
+
+    // Send modelview
     C3D_Mtx baseview;
     Mtx_Identity(&baseview);
     /*Mtx_RotateZ(&baseview, M_PI, true);
     Mtx_RotateX(&baseview, -M_PI / 2, true);
     Mtx_RotateY(&baseview, M_PI, true);*/
     Mtx_Translate(&baseview, 0.0, -10.0, -200.0, true);
-  
+
     C3D_Mtx camMat;
     //getBoneMat(&camProxy, row, &camMat, 3);
     //Mtx_Inverse(&camMat);
@@ -272,10 +272,10 @@ void effectSignScrollerRender(C3D_RenderTarget* targetLeft, C3D_RenderTarget* ta
     Mtx_Multiply(&skyview, &baseview, &camMat);
     Mtx_RotateZ(&skyview, row * 0.05, true);
 
-     // Left eye 
-    C3D_FrameDrawOn(targetLeft);  
+     // Left eye
+    C3D_FrameDrawOn(targetLeft);
     C3D_RenderTargetClear(targetLeft, C3D_CLEAR_ALL, 0x404040FF, 0);
-    
+
     // Uniform setup
     Mtx_PerspStereoTilt(&projection, 20.0f*M_PI/180.0f, 400.0f/240.0f, 0.01f, 6000.0f, -iod,  7.0f, false);
     C3D_FVUnifMtx4x4(GPU_VERTEX_SHADER, uLocProjection, &projection);
@@ -284,15 +284,15 @@ void effectSignScrollerRender(C3D_RenderTarget* targetLeft, C3D_RenderTarget* ta
     // Dispatch drawcalls
     drawModel(&modelSignpost, row);
     drawModel(&modelSign, row);
-    skyboxCubeImmediate(&texSky, 1000.0f, vec3(0.0f, 0.0f, 0.0f), &skyview, &projection); 
+    skyboxCubeImmediate(&texSky, 1000.0f, vec3(0.0f, 0.0f, 0.0f), &skyview, &projection);
 
     // Do fading
     //fade();
 
     // Right eye?
     if(iod > 0.0) {
-        C3D_FrameDrawOn(targetRight); 
-        C3D_RenderTargetClear(targetRight, C3D_CLEAR_ALL, 0x00ff00FF, 0); 
+        C3D_FrameDrawOn(targetRight);
+        C3D_RenderTargetClear(targetRight, C3D_CLEAR_ALL, 0x00ff00FF, 0);
 
         // Uniform setup
         Mtx_PerspStereoTilt(&projection, 20.0f*M_PI/180.0f, 400.0f/240.0f, 0.01f, 6000.0f, iod, 7.0f, false);
@@ -302,11 +302,11 @@ void effectSignScrollerRender(C3D_RenderTarget* targetLeft, C3D_RenderTarget* ta
         // Dispatch drawcalls
         drawModel(&modelSignpost, row);
         //drawModel(&modelSign, row);
-        skyboxCubeImmediate(&texSky, 1000.0f, vec3(0.0f, 0.0f, 0.0f), &skyview, &projection); 
+        skyboxCubeImmediate(&texSky, 1000.0f, vec3(0.0f, 0.0f, 0.0f), &skyview, &projection);
 
         // Perform fading
         //fade();
-    } 
+    }
 
     // Swap
     C3D_FrameEnd(0);
